8416e45ed686af83b0ef967e18ef431358796204
jacobplumblambda <jacob.plumb@lambdaschool.com>
2021-08-19 19:43:55 -0400

    added components to app component

---
+++ src/components/ContactForm.js
@@ -0,0 +1,14 @@
+import { Card, CardContent, CardHeader } from '@material-ui/core'
+
+const ContactForm = (props) => {
+    return (
+        <Card>
+            <CardHeader title="Add Contact"/>
+            <CardContent>
+                stuff
+            </CardContent>
+        </Card>
+    );
+}
+
+export default ContactForm;
---
+++ src/components/ContactList.js
@@ -0,0 +1,14 @@
+import { Card, CardContent, CardHeader } from '@material-ui/core'
+
+const ContactList = (props) => {
+    return (
+        <Card>
+            <CardHeader title="Contacts"/>
+            <CardContent>
+                stuff
+            </CardContent>
+        </Card>
+    );
+}
+
+export default ContactList;
